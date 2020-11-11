7fe509ea79418c11341b304add2781e75556f784
Ingmar Böschen <55027155+ingmarboeschen@users.noreply.github.com>
2020-11-11 11:55:23 +0100

    Add files via upload

--- R/character_get.alpha.error.R
+++ R/character_get.alpha.error.R
@@ -7,16 +7,18 @@
 #' x<-c("The threshold for significance was adjusted to .05/2",
 #' "Type 1 error rate was alpha=.05.")
 #' get.alpha.error(x)
-#' x<-"We display 95% CIs and use an alpha error of .05/5."
+#' x<-c("We used p<.05 as level of significance.",
+#'      "We display .95 CIs and use an adjusted alpha of .10/3.",
+#'      "The effect was significant with p<.025.")
 #' get.alpha.error(x)
 
 get.alpha.error<-function(x){
 x<-grep("[Mm]eta[a ]|[Mm]eta-|[Aa]mplitude|Hz",x,invert=TRUE,value=TRUE)
 split<-lapply(x,function(x) unlist(strsplit(x," and | with |, ")))
 # tolower
-lower<-lapply(split,tolower)
+lower<-unlist(lapply(split,tolower))
 # select lines 
-alpha1<-lapply(lower,function(x) grep("alpha error|type 1 error|type i error|level for significance|level of significance|significance level|cutoff|threshold|corrected|correction|adjust",x,value=TRUE))
+alpha1<-(lapply(lower,function(x) grep("alpha of|alpha error|type 1 error|type i error|level for significance|level of significance|significance level|cutoff|threshold|corrected|correction|adjust|confidence[- ]interval|[^a-z]ci[^a-rt-z]",x,value=TRUE)))
 # select lines with numbers
 alpha1<-lapply(alpha1,function(x) grep("[0-9]",x,value=TRUE))
 # remove "-'"
@@ -51,10 +53,6 @@ alpha1<-lapply(alpha1,function(x) gsub(".*probability of |.*cutoff of |.*cutoff 
 # remove non numbers at end
 alpha1<-lapply(alpha1,function(x){while(length(grep("[^0-9]$",x))>0){x<-gsub("[^0-9]$","",x)}
 return(x)})
-# remove p<... if has alpha=
-#alpha1<-lapply(alpha1,function(x){
-#if(length(grep("alpha",x))>0&length(grep("p[<]",x))>0) x<-gsub("p<....","",x)
-#})
 
 # select lines with numbers
 alpha1<-lapply(alpha1,function(x) grep("[0-9]",x,value=TRUE))
@@ -65,7 +63,7 @@ alpha1<-lapply(alpha1,function(x) gsub("p<","a=",x))
 
 # shorten string
 alpha<-lapply(alpha1,function(x) substr(x,1,12))
-# select lines with nuber only
+# select lines with number only
 alpha<-lapply(alpha,function(x) grep("[0-9]",x,value=TRUE))
 # remove from first space
 alpha<-lapply(alpha,function(x) gsub(" .*","",x))
@@ -91,10 +89,21 @@ return(x)
 })
 
 # remove lines with "\\.[2-9]"
-alpha<-lapply(alpha,function(x) grep("\\.[2-9]",x,value=TRUE,invert=TRUE))
+#alpha<-lapply(alpha,function(x) grep("\\.[2-9]",x,value=TRUE,invert=TRUE))
 
+# extract lines with corrected p-value
+corrected<-NULL
+i<-grep("/|adjust|correct",lower)
+if(length(i)>0){
+   corrected[i]<-alpha[i]}else corrected<-NA
+# set lines in alpha without "/" but "adjust" to NA
+i<-!unlist(grepl("/",lower))&unlist(grepl("adjust|correct",lower))
+if(sum(i)>0){
+alpha[i]<-NA
+}   
+   
 # convert "/" to decimal 
-alpha<-lapply(alpha,function(x){
+corrected<-lapply(corrected,function(x){
 if(length(grep("/",x))>0){
 pre<-suppressWarnings(as.numeric(lapply(x[grep("/",x)],function(y) gsub(".*=","",gsub("/.*","",y)))))
 post<-suppressWarnings(as.numeric(lapply(x[grep("/",x)],function(y) gsub(".*/","",gsub("([0-9])=.*","\\1",y)))))
@@ -106,53 +115,62 @@ x<-x[pre<=.1]
 return(x)}
 )
 
+# delete after /num
+alpha<-lapply(alpha,function(x) gsub("/[\\.0-9]*","",x))
 # delete front
 alpha<-lapply(alpha,function(x) gsub(".*[=<]","",x))
-# unique
-alpha<-lapply(alpha,unique)
+corrected<-lapply(corrected,function(x) gsub(".*[=<]","",x))
 # numerise and round
 alpha<-lapply(alpha,function(x) round(suppressWarnings(as.numeric(x)),5))
-# only include 0 < alpha <= .1 and not NA
-alpha<-lapply(alpha,function(x) x[x>0&x<=.1])
+corrected<-lapply(corrected,function(x) round(suppressWarnings(as.numeric(x)),5))
+# only include 0 < alpha <= 1 and not NA
+alpha<-lapply(alpha,function(x) x[x>0&x<=1])
+corrected<-lapply(corrected,function(x) x[x>0&x<=1])
 alpha<-lapply(alpha,function(x) x[!is.na(x)])
+corrected<-lapply(corrected,function(x) x[!is.na(x)])
+# unique
+alpha<-unique(unlist(alpha))
+corrected<-unique(unlist(corrected))
 
 ## get alpha from 1-x% Confidence Intervall
 # split x and lowerise
-split<-lapply(x,function(x) tolower(unlist(strsplit(x," and | with |, |; |\\("))))
+#split<-lapply(x,function(x) strsplit(x," and | with |, |; |\\("))
 # select lines with ci
-ci<-lapply(split,function(x) grep("[^a-z]ci[^a-rt-z]|confidence[ \\-]intervall",x,value=TRUE))
+ci<-lapply(lower,function(x) grep("[^a-z]ci[^a-rt-z]|[^a-z]ci$|confidence[ \\-]intervall",x,value=TRUE))
 # select lines with numbers
 ci<-lapply(ci,function(x) grep("[0-9]",x,value=TRUE))
-# select lines with %
-ci<-lapply(ci,function(x) grep("[%]",x,value=TRUE))
+# select lines with % or .num
+ci<-lapply(ci,function(x) grep("[\\%]|\\.[987]",x,value=TRUE))
 # remove space in front of %
 ci<-lapply(ci,function(x) gsub(" [%]","%",x))
 # remove text after %
 ci<-lapply(ci,function(x) gsub("[%].*","%",x))
+# remove text after .[0-9]
+ci<-lapply(ci,function(x) gsub(".*(\\.[0-9]*).*","\\1",x))
 # remove till space or special characters
 ci<-lapply(ci,function(x) gsub(".*[ ,;]","",x))
 # select lines without "-"
 ci<-lapply(ci,function(x) grep("\\-",x,value=TRUE,invert=TRUE))
 # remove letters
-ci<-lapply(ci,function(x) gsub("[a-z\\=]","",x))
+ci<-unlist(lapply(ci,function(x) gsub("[a-z\\=]","",x)))
 # remove ><
 #ci<-lapply(ci,function(x) gsub(".*[ \\(=<>\\-\\+;]","",x))
-
 # select lines starting with "[8-9][0-9]"
-ci<-lapply(ci,function(x) grep("^[8-9][0-9]",x,value=TRUE))
+ci<-lapply(ci,function(x) grep("^[7-9]|^\\.[7-9]",x,value=TRUE))
 # remove further punctuation
 ci<-lapply(ci,function(x) gsub(".*[<>\\{]","",x))
 # % to number
 ci<-lapply(ci,function(x) gsub("[%]","e-02",x))
 
 ci<-lapply(ci,function(x) round(1-as.numeric(x),4))
-
-ci<-lapply(ci,unique)
-
-table(unlist(ci))
+ci<-unique(unlist(ci))
 
 #####
-alpha<-unique(unlist(mapply(c,alpha,ci)))
+
 # return unique alpha errors
-return(as.vector(alpha))
+if(length(alpha)==0) alpha<-NULL
+if(length(corrected)==0) corrected<-NULL
+if(length(ci)==0) ci<-NULL
+
+return(list(alpha_error=alpha,corrected_alpha=corrected,alpha_from_CI=ci))
 }
